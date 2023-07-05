e6471ba9d1d1231d655ab0242caa9b8f6f29c4c6
Iryna Tsupryk <iranaumovich0@gmail.com>
2023-07-05 22:54:11 +0200

    обновить регулярное выражение для URL

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,4 +1,5 @@
-const urlRegex = /^((https?:\/\/(www\.)?)|(www\.))[\w\d-/]+\.[\w\d-/]+?[-._~:/?#[\]@!$&'()*+,;=]?$/i;
+const urlRegex =
+  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/i;
 
 module.exports = {
   urlRegex,
